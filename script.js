3b1e76d769008bab0cd07eb2e85fbbdc0d8f6049
kobe shaw <kobeshaw330@gmail.com>
2023-05-08 10:07:49 -0500

    added a function to the buttons to see if they would work and routed them to the neww page

---
+++ script.js
@@ -0,0 +1,31 @@
+/*difficulty buttons*/
+
+
+
+document.getElementById("easy-btn").addEventListener('click', function(){
+    setText("easy");
+});
+
+document.getElementById("medium-btn").addEventListener('click', function(){
+    setText("medium");
+});
+
+document.getElementById("hard-btn").addEventListener('click', function(){
+    setText("hard");
+});
+
+function setText(difficulty){
+    console.log("setText() called with difficulty: " + difficulty)
+    let text;
+    if(difficulty === "easy"){
+        text = 'This is an easy text to type.'
+    }else if (difficulty === "medium"){
+        text = 'This is an medium text to type.'
+    }else if (difficulty === "hard"){
+        text = 'This is an hard text to type.'
+    }
+    document.getElementById("text-container").textContent = text;
+
+    localStorage.setItem("text", text); // save text to local storage
+    window.location.href = "TypeingPage.html"; // redirect to typing page
+}
